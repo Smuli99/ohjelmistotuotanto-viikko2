338842306c609e80ee07fdbbaee5335dd57294b4
Smuli99 <hytonensamu99@gmail.com>
2025-11-06 13:51:04 +0200

    Tehtävä 8 tehty valmiiksi

--- osa2/project-reader/src/index.py
+++ osa2/project-reader/src/index.py
@@ -2,9 +2,9 @@ from project_reader import ProjectReader
 
 
 def main():
-    url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml"
-    reader = ProjectReader(url)
-    print(reader.get_project())
+    url = "https://raw.githubusercontent.com/ohjelmistotuotanto-jyu/tehtavat/refs/heads/main/osa2/test-project/pyproject.toml"
+    project = ProjectReader(url).get_project()
+    print(project)
 
 
 if __name__ == "__main__":
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,3 +1,4 @@
+import toml
 from urllib import request
 from project import Project
 
@@ -9,7 +10,15 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        data = toml.loads(content)
+
+        # pilkotaan data
+        name = data["tool"]["poetry"]["name"]
+        description = data["tool"]["poetry"]["description"]
+        license = data["tool"]["poetry"]["license"]
+        authors = data["tool"]["poetry"]["authors"]
+        dependencies = data["tool"]["poetry"]["dependencies"]
+        dev_dependecies = data["tool"]["poetry"]["group"]["dev"]["dependencies"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, authors, dependencies, dev_dependecies)
